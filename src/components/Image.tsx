434a75eb2e885cc3e6c20cf5629b64fa4106f481
GraduS <ILikeCherryJam@gmail.com>
2020-07-10 13:22:17 +0300

    Исправлен файл Image.tsx

--- src/components/Image.tsx
+++ src/components/Image.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import natureImg from '../assets/nature.jpg';
 
-const Image: React.FC = () => <img src={natureImg}></img>;
+const Image: React.FC = () => <img alt="" src={natureImg} />;
 
 export default Image;
